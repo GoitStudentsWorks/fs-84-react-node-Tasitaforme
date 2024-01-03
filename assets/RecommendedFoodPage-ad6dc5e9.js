b5a36b7774ef66715ae3d907127db9bfb5798bac
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-01-03 08:09:41 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/fs-84-react-node-Tasitaforme@34d89525d1dc6810a503db17fb7e041c5f905761 🚀

---
+++ assets/RecommendedFoodPage-ad6dc5e9.js
@@ -0,0 +1,85 @@
+import{s as n,H as r,u as c,V as x,r as a,j as e,C as m,t as p,q as l,A as g}from"./index-b3078306.js";import{g as f,a as h,b as u,c as b,d as w}from"./getRecommendedFood-d0ef8dc3.js";const j="/fs-84-react-node-Tasitaforme/assets/food-desk@1x-a984dc19.png",k="/fs-84-react-node-Tasitaforme/assets/food-desk@2x-1318a229.png",F="/fs-84-react-node-Tasitaforme/assets/food-tab@1x-20ca8956.png",T="/fs-84-react-node-Tasitaforme/assets/food-tab@2x-f20df540.png",R="/fs-84-react-node-Tasitaforme/assets/food-mob@1x-411148cb.png",v="/fs-84-react-node-Tasitaforme/assets/food-mob@2x-5a8c6e43.png",I=n.section`
+  padding-top: 16px;
+  padding-bottom: 40px;
+
+  @media screen and (min-width: 834px) {
+    padding-top: 24px;
+    padding-bottom: 100px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-top: 20px;
+    padding-bottom: 60px;
+  }
+`,$=n(r)`
+  margin-bottom: 16px;
+
+  @media screen and (min-width: 834px) {
+    margin-bottom: 20px;
+  }
+`,y=n.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 24px;
+
+  @media screen and (min-width: 1440px) {
+    flex-direction: row;
+    column-gap: 44px;
+    justify-content: flex-start;
+    align-items: flex-start;
+  }
+`,L=n.div`
+  width: 300px;
+  height: 312px;
+  flex-shrink: 0;
+
+  background-image: url(${R});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  @media (min-device-pixel-ratio: 2),
+    (min-resolution: 192dpi),
+    (min-resolution: 2dppx) {
+    background-image: url(${v});
+  }
+
+  @media screen and (min-width: 834px) {
+    width: 380px;
+    height: 396px;
+    background-image: url(${F});
+
+    @media (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url(${T});
+    }
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 536px;
+    height: 560px;
+    background-image: url(${j});
+    margin-left: 116px;
+    @media (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url(${k});
+    }
+  }
+`,S=n.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+
+  @media screen and (min-width: 834px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    column-gap: 20px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 676px;
+  }
+`;function C(){const i=c(x),[d,s]=a.useState([]),t=f(d,10);return a.useEffect(()=>{i&&h().then(o=>{s(o)}).catch(o=>console.log(o))},[i]),e.jsx("main",{children:e.jsx(I,{children:e.jsxs(m,{children:[e.jsx($,{children:"Recommended food"}),e.jsxs(y,{children:[e.jsx(L,{alt:"Illustration of women with food",width:"536px"}),d.length===0?e.jsx(p,{}):e.jsx(S,{children:t.map(o=>e.jsxs(u,{children:[e.jsx("img",{src:o.img,alt:o.name,width:"46px"}),e.jsxs("div",{children:[e.jsx(b,{children:o.name}),e.jsxs(w,{children:[e.jsx(l,{children:o.amount}),e.jsxs(g,{children:[o.calories," calories"]})]})]})]},o._id))})]})]})})})}export{C as default};
---
+++ assets/SignUpPage-3b94196e.js
@@ -0,0 +1,230 @@
+import{j as e,s,a as P,H as l,e as T,f as j,g as D,r as $,F as y,d as w,h as B,B as m,i as p,k as C,m as E,u as V,b as z,c as Y,o as Z,p as O,n as N,C as _}from"./index-b3078306.js";import{B as g,F as J,I as K,T as Q,S as X,a as ee,b as ae}from"./SignUpImage.styled-58816b3f.js";import{w as se,a as ie,b as te,c as ne,d as re,e as oe}from"./watch-desk@2x-74011178.js";import{S as ce,F as M,b as S,c as v,V as xe,I as de,a as k,d as pe,L as me}from"./LoadingModal-4a6e6a42.js";import"./SignInPage.styled-45c30fd6.js";const he="/fs-84-react-node-Tasitaforme/assets/hiking-desk@1x-867f5633.png",le="/fs-84-react-node-Tasitaforme/assets/hiking-desk@2x-e58c3520.png",ge="/fs-84-react-node-Tasitaforme/assets/hiking-tab@1x-1073b886.png",ue="/fs-84-react-node-Tasitaforme/assets/hiking-tab@2x-51fd2364.png",fe="/fs-84-react-node-Tasitaforme/assets/hiking-mob@1x-19e0c6c9.png",be="/fs-84-react-node-Tasitaforme/assets/hiking-mob@2x-1b2c3203.png",je="/fs-84-react-node-Tasitaforme/assets/fitness-desk@1x-02917009.png",ye="/fs-84-react-node-Tasitaforme/assets/fitness-desk@2x-9b02aeb0.png",we="/fs-84-react-node-Tasitaforme/assets/fitness-tab@1x-4e471a75.png",ke="/fs-84-react-node-Tasitaforme/assets/fitness-tab@2x-27b080de.png",ve="/fs-84-react-node-Tasitaforme/assets/fitness-mob@1x-942df0aa.png",Te="/fs-84-react-node-Tasitaforme/assets/fitness-mob@2x-5570cf98.png",$e="/fs-84-react-node-Tasitaforme/assets/parametrs-desk@1x-fb8dec06.png",Fe="/fs-84-react-node-Tasitaforme/assets/parametrs-desk@2x-ad138520.png",Me="/fs-84-react-node-Tasitaforme/assets/parameters-tab@1x-f269c222.png",Se="/fs-84-react-node-Tasitaforme/assets/parameters-tab@2x-25197ad8.png",Re="/fs-84-react-node-Tasitaforme/assets/parameters-mob@1x-fba6cca4.png",De="/fs-84-react-node-Tasitaforme/assets/parameters-mob@2x-a074fcff.png",Ee="/fs-84-react-node-Tasitaforme/assets/activity-desk@1x-a756ff63.png",Ae="/fs-84-react-node-Tasitaforme/assets/activity-desk@2x-268b7897.png",Be="/fs-84-react-node-Tasitaforme/assets/activity-tab@1x-c17492e3.png",Ne="/fs-84-react-node-Tasitaforme/assets/activity-tab@2x-d532ca7f.png",qe="/fs-84-react-node-Tasitaforme/assets/activity-mob@1x-96e8d684.png",Le="/fs-84-react-node-Tasitaforme/assets/activity-mob@2x-d105836c.png";function Ce({currentStep:a}){return e.jsxs(e.Fragment,{children:[a===1&&e.jsx(g,{$imageDesk1x:se,$imageDesk2x:ie,$imageTab1x:te,$imageTab2x:ne,$imageMob1x:re,$imageMob2x:oe,$heightMob:"296px",$heightTab:"376px",$heightDesk:"588px"}),a===2&&e.jsx(g,{$imageDesk1x:he,$imageDesk2x:le,$imageTab1x:ge,$imageTab2x:ue,$imageMob1x:fe,$imageMob2x:be,$heightMob:"290px",$heightTab:"368px",$heightDesk:"574px"}),a===3&&e.jsx(g,{$imageDesk1x:je,$imageDesk2x:ye,$imageTab1x:we,$imageTab2x:ke,$imageMob1x:ve,$imageMob2x:Te,$heightMob:"288px",$heightTab:"366px",$heightDesk:"570px"}),a===4&&e.jsx(g,{$imageDesk1x:$e,$imageDesk2x:Fe,$imageTab1x:Me,$imageTab2x:Se,$imageMob1x:Re,$imageMob2x:De,$heightMob:"302px",$heightTab:"382px",$heightDesk:"594px"}),a===5&&e.jsx(g,{$imageDesk1x:Ee,$imageDesk2x:Ae,$imageTab1x:Be,$imageTab2x:Ne,$imageMob1x:qe,$imageMob2x:Le,$heightMob:"304px",$heightTab:"384px",$heightDesk:"598px"})]})}const u=s(P)`
+  font-size: 18px;
+  color: var(--Color-Primary-Grey, #b6b6b6);
+  margin-top: 16px;
+
+  @media screen and (min-width: 834px) {
+    font-size: 22px;
+    text-align: center;
+  }
+
+  @media screen and (min-width: 1440px) {
+    text-align: start;
+  }
+`;function Ie({currentStep:a}){return e.jsxs(e.Fragment,{children:[a===1&&e.jsxs(e.Fragment,{children:[e.jsx(l,{children:"Sign up"}),e.jsx(u,{children:"You need to register to use the service"})]}),a===2&&e.jsxs(e.Fragment,{children:[e.jsx(l,{children:"Your goal"}),e.jsx(u,{children:"Choose a goal so that we can help you effectively"})]}),a===3&&e.jsxs(e.Fragment,{children:[e.jsx(l,{children:"Select gender and age"}),e.jsxs(u,{children:["Enter your gender and age for correct ",e.jsx("br",{}),"performance tracking"]})]}),a===4&&e.jsxs(e.Fragment,{children:[e.jsx(l,{children:"Body parameters"}),e.jsx(u,{children:"Enter your parameters for correct performance tracking"})]}),a===5&&e.jsxs(e.Fragment,{children:[e.jsx(l,{children:"Your Activity"}),e.jsxs(u,{children:["To correctly calculate calorie ",e.jsx("br",{}),"and water intake"]})]})]})}const Ue=/^(([^<>()[\]\\.,;:\s@"]+(.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/,Ge=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/,We=T().shape({name:j().required("Required").min(2,"Too Short!").max(70,"Too Long!"),email:j().required("Required").email("Invalid email").matches(Ue,"Invalid email address"),password:j().required("Required").min(7,"Min length: 7 characters").matches(Ge,"Use uppercase, lowercase letters, number. Letters only in English.")}),He=T().shape({goal:j().required("Required, choose one of three")}),Pe=T().shape({gender:j().required("Required, choose one of two"),age:D().required("Required").min(1,"Age can not be less than 1").max(90,"Age can not be more than 90").typeError("Age must be a number")}),Ve=T().shape({height:D().required("Required").min(140,"Height can not be less than 140").max(240,"Height can not be more than 240").typeError("Height must be a number"),weight:D().required("Required").min(40,"Weight can not be less than 40").max(220,"Weight can not be more than 220").typeError("Weight must be a number")});function ze({handleNextStep:a,handleSubmit:r,userData:c}){const[t,i]=$.useState(!1),n=o=>{r(o),a()};return e.jsx(y,{initialValues:c,validationSchema:We,onSubmit:n,children:({errors:o,touched:d})=>e.jsxs(ce,{children:[e.jsxs(M,{children:[e.jsx(S,{type:"text",name:"name",placeholder:"Name",className:d.name?o.name?"input-error":"input-success":"input-normal"}),e.jsx(v,{component:"p",name:"name"})]}),e.jsxs(M,{children:[e.jsx(S,{type:"email",name:"email",placeholder:"E-mail",className:d.email?o.email?"input-error":"input-success":"input-normal"}),e.jsx(v,{component:"p",name:"email"})]}),e.jsxs(M,{children:[e.jsx(S,{type:t?"text":"password",name:"password",placeholder:"Password",autoComplete:"off",className:d.password?o.password?"input-error":"input-success":"input-normal"}),e.jsx(v,{component:"p",name:"password"}),!o.password&&d.password&&e.jsx(xe,{children:"Password is secure"}),e.jsxs(de,{onMouseEnter:()=>i(!0),onMouseLeave:()=>i(!1),children:[d.password&&!o.password&&e.jsx(k,{width:"32px",height:"32px",children:e.jsx("use",{href:`${w}#correct`})}),d.password&&o.password&&e.jsx(k,{width:"32px",height:"32px",children:e.jsx("use",{href:`${w}#error`})}),!d.password&&!t&&e.jsx(k,{width:"32px",height:"32px",children:e.jsx("use",{href:`${w}#eye-off`})}),t&&!d.password&&e.jsx(k,{width:"32px",height:"32px",children:e.jsx("use",{href:`${w}#eye`})})]})]}),e.jsx(pe,{type:"submit",children:"Next"})]})})}const Ye=s(B)`
+  margin-top: 24px;
+
+  @media screen and (min-width: 834px) {
+    padding: 0px 32px 0px 32px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding: 0px 232px 0px 0px;
+  }
+`,R=s.label`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  &:not(:first-child) {
+    margin-top: 24px;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  @media screen and (min-width: 834px) {
+    &:not(:first-child) {
+      margin-top: 0px;
+    }
+    width: 110px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    &:not(:first-child) {
+      margin-top: 16px;
+    }
+  }
+`,Ze=s.div`
+  position: relative;
+  @media screen and (min-width: 834px) {
+    display: flex;
+    gap: 24px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    flex-direction: column;
+    gap: 0px;
+  }
+`,Oe=s(m)`
+  width: 100%;
+  margin-top: 24px;
+`,I=s(m)`
+  width: 192px;
+  height: 20px;
+  margin: 20px auto 0;
+  font-weight: 400;
+  background-color: transparent;
+  color: var(--Color-Primary-Grey, #b6b6b6);
+
+  transition: color var(--transition-dur-and-fun);
+
+  &:hover,
+  &:focus {
+    box-shadow: none;
+    color: var(--green-light);
+  }
+`,_e=s(v)`
+  top: -32px;
+  left: 20px;
+`;function Je({handleNextStep:a,handleSubmit:r,userData:c,handlePrevStep:t}){const i=n=>{r(n),a()};return e.jsx(y,{initialValues:c,validationSchema:He,onSubmit:i,children:()=>e.jsxs(Ye,{children:[e.jsxs(Ze,{children:[e.jsxs(R,{children:[e.jsx(p,{type:"radio",name:"goal",value:"Lose fat"}),"Lose Fat"]}),e.jsxs(R,{children:[e.jsx(p,{type:"radio",name:"goal",value:"Maintain"}),"Maintain"]}),e.jsxs(R,{children:[e.jsx(p,{type:"radio",name:"goal",value:"Gain muscle"}),"Gain Muscle"]}),e.jsx(_e,{component:"p",name:"goal"})]}),e.jsx(Oe,{type:"submit",children:"Next"}),e.jsx(I,{onClick:t,children:"Back"})]})})}const U=s(B)`
+  margin-top: 24px;
+  width: 100%;
+
+  @media screen and (min-width: 834px) {
+    padding: 0px 32px 0px 32px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding: 0px 232px 0px 0px;
+    width: 444px;
+  }
+`,G=s(m)`
+  width: 192px;
+  height: 20px;
+  margin: 20px auto 0;
+  font-weight: 400;
+  background-color: transparent;
+  color: var(--Color-Primary-Grey, #b6b6b6);
+  transition: color var(--transition-dur-and-fun);
+
+  &:hover,
+  &:focus {
+    box-shadow: none;
+    color: var(--green-light);
+  }
+`,Ke=s(m)`
+  width: 100%;
+  margin-top: 40px;
+`,Qe=s.p`
+  font-weight: 500;
+`,Xe=s.div`
+  position: relative;
+
+  margin-top: 12px;
+  display: flex;
+  gap: 16px;
+
+  @media screen and (min-width: 1440px) {
+    flex-direction: column;
+  }
+`,q=s.label`
+  width: 140px;
+  display: flex;
+  gap: 8px;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+  }
+`,ea=s.label`
+  margin-top: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  font-weight: 500;
+  position: relative;
+`,A=s(C)`
+  position: absolute;
+  left: 10px;
+  top: 58px;
+`,aa=s(C)`
+  position: absolute;
+  top: -26px;
+  left: 20px;
+`;function sa({handleNextStep:a,handleSubmit:r,userData:c,handlePrevStep:t}){const i=n=>{r({...n,age:Number(n.age)}),a()};return e.jsx(y,{initialValues:c,validationSchema:Pe,onSubmit:i,children:({errors:n,touched:o})=>e.jsxs(U,{children:[e.jsx(Qe,{children:"Gender"}),e.jsxs(Xe,{children:[e.jsxs(q,{children:[e.jsx(p,{type:"radio",name:"gender",value:"Male"}),"Male"]}),e.jsxs(q,{children:[e.jsx(p,{type:"radio",name:"gender",value:"Female"}),"Female"]}),e.jsx(aa,{component:"p",name:"gender"})]}),e.jsxs(ea,{children:["Your age",e.jsx(E,{type:"text",name:"age",placeholder:"Enter your age",className:o.age?n.age?"input-error":"input-success":"input-normal"}),e.jsx(A,{component:"p",name:"age"})]}),e.jsx(Ke,{type:"submit",children:"Next"}),e.jsx(G,{onClick:t,children:"Back"})]})})}const ia=s(B)`
+  margin-top: 24px;
+  width: 100%;
+
+  @media screen and (min-width: 834px) {
+    padding: 0px 32px 0px 32px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding: 0px 232px 0px 0px;
+  }
+`,L=s.label`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  font-weight: 500;
+  position: relative;
+
+  &:not(:first-child) {
+    margin-top: 24px;
+  }
+`,ta=s(m)`
+  margin-top: 40px;
+  width: 100%;
+`;function na({handleNextStep:a,handleSubmit:r,userData:c,handlePrevStep:t}){const i=n=>{r({...n,weight:Number(n.weight),height:Number(n.height)}),a()};return e.jsx(y,{initialValues:c,validationSchema:Ve,onSubmit:i,children:({errors:n,touched:o})=>e.jsxs(ia,{children:[e.jsxs(L,{children:["Height",e.jsx(E,{type:"text",name:"height",placeholder:"Enter your height",className:o.height?n.height?"input-error":"input-success":"input-normal"}),e.jsx(A,{component:"p",name:"height"})]}),e.jsxs(L,{children:["Weight",e.jsx(E,{type:"text",name:"weight",placeholder:"Enter your weight",className:o.weight?n.weight?"input-error":"input-success":"input-normal"}),e.jsx(A,{component:"p",name:"weight"})]}),e.jsx(ta,{type:"submit",children:"Next"}),e.jsx(G,{onClick:t,children:"Back"})]})})}const ra=s(U)`
+  position: relative;
+
+  @media screen and (min-width: 834px) {
+    padding: 0px;
+    width: 460px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 100%;
+    padding: 0px 116px 0px 0px;
+  }
+`,f=s.label`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+
+  &:not(:first-child) {
+    margin-top: 16px;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+`,oa=s(m)`
+  width: 100%;
+  margin-top: 24px;
+
+  @media screen and (min-width: 834px) {
+    width: 380px;
+    margin: 16px auto 0;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 212px;
+    margin: 16px 0 0;
+  }
+`,ca=s(I)`
+  @media screen and (min-width: 1440px) {
+    width: 212px;
+    margin-left: 0;
+  }
+`,xa=s.p`
+  position: absolute;
+  top: -20px;
+  left: 18px;
+  font-size: 12px;
+  color: #e74a3b;
+`,b=s.p`
+  display: block;
+  width: 280px;
+
+  @media screen and (min-width: 834px) {
+    width: 438px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 308px;
+  }
+`;function da({userData:a,handlePrevStep:r,handleSubmitForm:c,handleChange:t}){const[i,n]=$.useState(!1),o=()=>{i||c()},d=x=>{n(x)};return e.jsx(y,{initialValues:a,onSubmit:o,children:()=>e.jsxs(ra,{children:[e.jsxs(f,{children:[e.jsx(p,{onChange:x=>t(Number(x.currentTarget.value)),type:"radio",name:"activityRatio",value:"1.2",checked:a.activityRatio===1.2}),e.jsx(b,{children:"1.2 - if you do not have physical activity and sedentary work"})]}),e.jsxs(f,{children:[e.jsx(p,{onChange:x=>t(Number(x.currentTarget.value)),type:"radio",name:"activityRatio",value:"1.375",checked:a.activityRatio===1.375}),e.jsx(b,{children:"1.375 - if you do short runs or light gymnastics 1-3 times a week"})]}),e.jsxs(f,{children:[e.jsx(p,{onChange:x=>t(Number(x.currentTarget.value)),type:"radio",name:"activityRatio",value:"1.55",checked:a.activityRatio===1.55}),e.jsx(b,{children:"1.55 - if you play sports with average loads 3-5 times a week"})]}),e.jsxs(f,{children:[e.jsx(p,{onChange:x=>t(Number(x.currentTarget.value)),type:"radio",name:"activityRatio",value:"1.725",checked:a.activityRatio===1.725}),e.jsx(b,{children:"1.725 - if you train fully 6-7 times a week"})]}),e.jsxs(f,{children:[e.jsx(p,{onChange:x=>t(Number(x.currentTarget.value)),type:"radio",name:"activityRatio",value:"1.9",checked:a.activityRatio===1.9}),e.jsx(b,{children:"1.9 - if your work is related to physical labor, you train 2 times a day and include strength exercises in your training program"})]}),i&&a.activityRatio===""&&e.jsx(xa,{children:"Required, choose one of five"}),e.jsx(oa,{type:"submit",onClick:()=>{a.activityRatio===""?d(!0):d(!1)},children:"Sign Up"}),e.jsx(ca,{onClick:r,children:"Back"})]})})}function pa({currentStep:a,handleNextStep:r,handlePrevStep:c}){const{isLoading:t}=V(z),[i,n]=$.useState({name:"",email:"",password:"",goal:"",gender:"",age:"",height:"",weight:"",activityRatio:""}),o=Y(),d=Z(),x=h=>{n(F=>({...F,...h}))},W=h=>{n(F=>({...F,activityRatio:h}))},H=async()=>{try{await o(O(i)).unwrap(),N.success(`You have successfully signed up! 
+ A confirmation letter has been sent to your email, please check it!`),d("/signin")}catch(h){N.error(`Something went wrong! 
+ ${h.message}!`)}};return e.jsxs(e.Fragment,{children:[a===1&&e.jsx(ze,{handleNextStep:r,handleSubmit:x,userData:i}),a===2&&e.jsx(Je,{handleNextStep:r,handleSubmit:x,userData:i,handlePrevStep:c}),a===3&&e.jsx(sa,{handleNextStep:r,handleSubmit:x,userData:i,handlePrevStep:c}),a===4&&e.jsx(na,{handleNextStep:r,handleSubmit:x,userData:i,handlePrevStep:c}),a===5&&e.jsx(da,{userData:i,handlePrevStep:c,handleSubmitForm:H,handleChange:W}),t&&e.jsx(me,{isOpen:t})]})}function fa(){const[a,r]=$.useState(1),c=()=>{r(i=>i+1)},t=()=>{r(i=>i-1)};return e.jsx("main",{children:e.jsx(_,{children:e.jsxs(J,{children:[e.jsx(K,{children:e.jsx(Ce,{currentStep:a})}),e.jsxs(Q,{children:[e.jsx(Ie,{currentStep:a}),e.jsx(pa,{currentStep:a,handleNextStep:c,handlePrevStep:t}),a===1&&e.jsxs(X,{children:[e.jsx(ee,{children:"Do you already have an account?"}),e.jsx(ae,{to:"/signin",children:"Sign in"})]})]})]})})})}export{fa as default};
